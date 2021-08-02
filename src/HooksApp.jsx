621ecca79aac82876803b90a74acf49a61a6b936
David Merchan <juan1014166@gmail.com>
2021-08-02 15:39:52 -0500

    hooks app start

---
+++ src/HooksApp.jsx
@@ -0,0 +1,13 @@
+// import PropTypes from 'prop-types';
+
+const HooksApp = (props) => {
+  return (
+    <>
+      <h1>Hooks App</h1>
+    </>
+  );
+};
+
+// HooksApp.propTypes = {	};
+
+export default HooksApp;
