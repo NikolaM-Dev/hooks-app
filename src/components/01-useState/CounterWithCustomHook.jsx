0d6a7f8af5f76982baec1f346b7ca082da0b7f6b
David Merchan <juan1014166@gmail.com>
2021-08-02 17:37:56 -0500

    useState - CounterWithCustomHook

---
+++ src/components/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,37 @@
+import useCounter from 'hooks/useCounter';
+
+import './CounterApp.css';
+
+const CounterWithCustomHook = () => {
+  const {
+    state: counter,
+    increment,
+    decrement,
+    reset,
+  } = useCounter({ initialState: 100 });
+
+  return (
+    <>
+      <h1>Counter with hook: {counter}</h1>
+      <hr />
+
+      <button
+        onClick={() => increment({ factor: 2 })}
+        className="btn btn-primary "
+      >
+        + 1
+      </button>
+      <button
+        onClick={() => decrement({ factor: 2 })}
+        className="btn btn-secondary"
+      >
+        - 1
+      </button>
+      <button onClick={reset} className="btn">
+        Reset
+      </button>
+    </>
+  );
+};
+
+export default CounterWithCustomHook;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import HooksApp from './HooksApp';
-import CounterApp from 'components/01-useState/CounterApp';
+// import CounterApp from 'components/01-useState/CounterApp';
+import CounterWithCustomHook from 'components/01-useState/CounterWithCustomHook';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>,
   document.getElementById('root')
 );
